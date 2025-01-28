27fe8e2f351c0a4f664d9827ccd9c8941e2aec8e
Isaac-Salas <iscprepa@gmail.com>
2025-01-28 15:05:15 -0600

    Test

--- Scripts/SnakeLevel/Portraitparticles.gd
+++ Scripts/SnakeLevel/Portraitparticles.gd
@@ -1,11 +1,12 @@
 extends Sprite2D
 class_name Portrait_Snake
-@onready var particles : GPUParticles2D = $GPUParticles2D
-const KIWI = preload("res://Assets/Sprites/Portraits/Fruit_Kiwi.png")
-const MANZANAI = preload("res://Assets/Sprites/Portraits/Fruit_Manzanai.png")
-const NARANJAI = preload("res://Assets/Sprites/Portraits/Fruit_Naranjai.png")
+@onready var particles : CPUParticles2D = $GPUParticles2D
+const CARA_FELIZ = preload("res://Assets/Sprites/SnakeLevel/CaraFeliz.png")
+const CARA_ENOJADA = preload("res://Assets/Sprites/SnakeLevel/CaraEnojada.png")
+@onready var shake_component = $ShakeComponent
 
-func choose_fruit(fruit):
-	particles.texture = fruit
+func choose_emotion(emotion):
+	
+	particles.texture = emotion
 	particles.restart()
 	particles.emitting = true

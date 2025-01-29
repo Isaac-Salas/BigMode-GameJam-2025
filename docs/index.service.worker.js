19704db709a43bf0c01042a3689858765b11729f
Isaac-Salas <iscprepa@gmail.com>
2025-01-28 22:51:30 -0600

    Build test

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1738098199|872882564';
+const CACHE_VERSION = '1738126268|7470104863';
 /** @type {string} */
 const CACHE_PREFIX = 'BigMode2025-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

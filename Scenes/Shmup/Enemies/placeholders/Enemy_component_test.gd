a5389647cfc46961a1fa45e986a83141a135430d
Isaac-Salas <iscprepa@gmail.com>
2025-01-30 17:47:58 -0600

    Some funny changes

---
+++ Scenes/Shmup/Enemies/placeholders/Enemy_component_test.gd
@@ -0,0 +1,19 @@
+class_name EnemyComponent
+extends RigidBody2D
+
+
+@onready var stats_component = %StatsComponent
+@onready var move_component = $MoveComponent
+ 
+
+
+#func _on_area_2d_area_entered(area):
+	#print(area.name)
+	#if area.name == "ShotHit":
+		#collision_layer = 512
+		#collision_mask = 512
+		#linear_velocity.x = randi_range(300,600)
+		#linear_velocity.y = randi_range(-400,400)
+		#angular_velocity = 100
+		#move_component.active = false
+		#set_deferred("freeze", false)

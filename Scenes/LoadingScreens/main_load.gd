d845a3283cb299ce72f0951b46f36e3669cc4e46
Isaac-Salas <iscprepa@gmail.com>
2025-01-29 21:36:38 -0600

    Test
    Buildtest again

--- Scenes/LoadingScreens/main_load.gd
+++ Scenes/LoadingScreens/main_load.gd
@@ -4,6 +4,7 @@ class_name LoadingScreen
 @export var ScenePath : String 
 @onready var scene_status = 0
 @onready var progress_bar = $ProgressBar
+@onready var loading = $Loading
 
 @onready var timer = $Timer
 @onready var thing : bool
@@ -15,6 +16,7 @@ func _ready():
 	ResourceLoader.load_threaded_request(ScenePath)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	loading.scale += Vector2(0.001,0.001)
 	scene_status = ResourceLoader.load_threaded_get_status(ScenePath,progress)
 	progress_bar.value = floor(progress[0]*100)
 	if scene_status == ResourceLoader.THREAD_LOAD_LOADED:
--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1738207439|1051019751';
+const CACHE_VERSION = '1738208179|368814140';
 /** @type {string} */
 const CACHE_PREFIX = 'BigMode2025-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

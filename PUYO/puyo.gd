5b25f201558a9d10fd3d81f428f20cf839998751
Alejandro Alonso Sánchez <49330757+alexcavadora@users.noreply.github.com>
2025-01-29 20:19:02 -0600

    some changes puyo kinda worky

--- PUYO/puyo.gd
+++ PUYO/puyo.gd
@@ -3,19 +3,39 @@ extends Node2D
 @onready var sprite : AnimatedSprite2D = $Sprite
 @export var status : String = "TETHERED"
 var color = randi() % 4 + 1 
+var isBomb = false
 var is_popping = false
 var grid_pos: Vector2i = Vector2i.ZERO
 enum {CLEAR, RED, BLUE, GREEN, YELLOW}
 #      0      1    2      3       4
+
+
 var target_fall = 0
 signal pop_finished(puyo)
 
 func _ready() -> void:
+	isBomb = (randi() % 100) % 17 == 0
 	match color:
-		1: sprite.play("red")
-		2: sprite.play("blue")
-		3: sprite.play("green")
-		4: sprite.play("yellow")
+		1:
+			if !isBomb:
+				sprite.play("red")
+			else:
+				sprite.play("red_bomb")
+		2:
+			if !isBomb:
+				sprite.play("blue")
+			else:
+				sprite.play("blue_bomb")
+		3:
+			if !isBomb:
+				sprite.play("green")
+			else:
+				sprite.play("green_bomb")
+		4:
+			if !isBomb:
+				sprite.play("yellow")
+			else:
+				sprite.play("yellow_bomb")
 
 func _on_puyo_free():
 	if is_popping:
@@ -37,14 +57,30 @@ func pop():
 		3: sprite.play("green_free")
 		4: sprite.play("yellow_free")
 
+func explode():
+	if is_popping:
+		return
+	is_popping = true
+	if isBomb:
+		match color:
+			1: sprite.play("red_bomb_explode")
+			2: sprite.play("blue_bomb_explode")
+			3: sprite.play("green_bomb_explode")
+			4: sprite.play("yellow_bomb_explode")
+	match color:
+		1: sprite.play("red_explode")
+		2: sprite.play("blue_explode")
+		3: sprite.play("green_explode")
+		4: sprite.play("yellow_explode")
+
 func _on_sprite_animation_finished() -> void:
 	if sprite.animation.ends_with("explode"):
-		emit_signal("pop_finished", self)  # Notify popping is complete
+		emit_signal("pop_finished", self)
 		queue_free()
 	if sprite.animation.ends_with("free"):
 		_ready()
 
 func _on_sprite_animation_looped() -> void:
 	if sprite.animation.ends_with("free"):
-		emit_signal("pop_finished", self)  # Notify popping is complete
+		emit_signal("pop_finished", self)
 		queue_free()

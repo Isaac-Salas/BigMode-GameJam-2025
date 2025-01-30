ed52fc85ad84b815c2cec483c7322b80f282204c
Isaac Salas <iscprepa@gmail.com>
2025-01-30 16:11:21 -0600

    Deleted .import and added shmup base

--- Components/scale_component.gd
+++ Components/scale_component.gd
@@ -1,6 +1,6 @@
 # Give the component a class name so it can be instanced as a custom node
-class_name ScaleComponent
 extends Node
+class_name ScaleComponent
 
 # Export the sprite that this component will be scaling
 @export var sprite: Node2D
